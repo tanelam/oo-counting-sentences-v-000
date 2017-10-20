103e340609186bba237cad8cbe200a6542016e4e
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-20 20:48:42 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,20 @@
-require 'pry'
+
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+  self.split(/[.?!]/).delete_if { |x| x.size < 3 }.size 
+    end
 end
